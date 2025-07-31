0caff94684d58a4947b14b4fd0f4bbd61e4e1a73
hamzamafarjeh <mafarjehamza@gmail.com>
2025-07-31 12:16:06 +0300

    init commit

---
+++ demo/src/main/java/com/example/demo/students.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private int age;
+    private String grade;
+    private String email;
+
+    // Getters and Setters
+}
