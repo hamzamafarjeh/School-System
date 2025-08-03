994f9a1b566410c6476b9f72018c80eee1ec53cb
hamzamafarjeh <mafarjehamza@gmail.com>
2025-08-03 16:53:39 +0300

    init commit

--- demo/src/main/java/com/example/demo/repository/srepository.java
+++ demo/src/main/java/com/example/demo/repository/srepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Student;
+import com.example.demo.model.students;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Integer> {
+public interface sRepository extends JpaRepository<students, Integer> {
         }
 
