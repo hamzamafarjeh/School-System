c8f43eedecac3a4e114e931f934b838691dcafc0
hamzamafarjeh <mafarjehamza@gmail.com>
2025-08-07 15:22:23 +0300

    init commit

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,24 +1,21 @@
-package com.example.school.controller;
+package com.example.demo.controller;
 
-import com.example.school.model.Student;
-import com.example.school.service.StudentService;
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
 
-    private final StudentService service;
+    @Autowired
+    private StudentRepository studentRepository;
 
-    public StudentController(StudentService service) {
-        this.service = service;
-    }
 
-    // GET /api/students
-    @GetMapping
-    public List<Student> getAllStudents() {
-        return service.getAllStudents();
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
     }
 }
+
