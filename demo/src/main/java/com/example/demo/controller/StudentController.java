5ac52984b56600ba73481d950889612753d8754b
hamzamafarjeh <mafarjehamza@gmail.com>
2025-08-13 14:45:28 +0300

    init commit

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.model.students;
 import com.example.demo.repository.srepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
@@ -15,13 +17,23 @@ public class StudentController {
 
 
     @PostMapping
-    public String createStudent() {
-        return "Hello World";
+    public students createStudent(@RequestBody students Student) {
+        return studentRepository.save(Student);
     }
 
     @GetMapping("/{id}")
-    public Optional<students> getStudent(@PathVariable int id) {
-        return studentRepository.findById(id);
+    public ResponseEntity<students> getStudentById(@PathVariable int id) {
+        Optional<students> optionalStudent = studentRepository.findById(id);
+
+        if (optionalStudent.isPresent()) {
+            // إذا الطالب موجود → رجعه مع status 200
+            return ResponseEntity.ok(optionalStudent.get());
+        } else {
+            // إذا مش موجود → رجع 404
+            return ResponseEntity.notFound().build();
+        }
     }
+
+
 }
 
